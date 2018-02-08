eae88dee27429c90e7c20539bd33db4e026e6b89
Lijun Ma <lijunm@uci.edu>
2018-02-07 17:43:09 -0800

    sort tile/year

--- project2/WebContent/search.js
+++ project2/WebContent/search.js
@@ -1,8 +1,9 @@
 
 var movieTableBodyElement = jQuery("#movie_table_body");
 var movieTableHeadElement = jQuery("#movie_head");
-var totalJsonSize, curItemsPerPage;
-var curPage = 0;
+var totalJsonSize, curItemsPerPage, curPage = 0;;
+var titleOrderAsc = 0, yearOrderAsc = 0;
+
 
 function splitListItems(listItems) {
 	console.log(listItems)
@@ -33,8 +34,6 @@ function handleSearchResult(resultDataString) {
 		showPage(curItemsPerPage);
 	}	
 }
-
-
 
 
 function showPage(itemsPerPage) {
@@ -73,8 +72,35 @@ function nextPage() {
 	if ((curPage + 1) * curItemsPerPage < totalJsonSize) {
 		curPage++;
 	}
-	showPage(curItemsPerPage) ;
+	showPage(curItemsPerPage);
 }
+
+function sortTable(n) {
+	//sort movie title
+	if (n == 1) {
+		resultDataJson.sort(function(a, b) {
+			if (titleOrderAsc == 0) {
+				return a.movie_title < b.movie_title;
+			} else {
+				return a.movie_title > b.movie_title;
+			}
+		});
+		titleOrderAsc = 1 - titleOrderAsc;
+	} 
+	//sort year
+	else {
+		resultDataJson.sort(function(a, b) {
+			if (yearOrderAsc == 0) {
+				return a.movie_year < b.movie_year;
+			} else {
+				return a.movie_year > b.movie_year;
+			}
+		});
+		yearOrderAsc = 1 - yearOrderAsc;
+	}
+	showPage(curItemsPerPage);
+}
+
 
 
 function getQueryString() {
@@ -99,60 +125,6 @@ function empty() {
     };
 }
 
-function sortTable(n) {
-	  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-	  table = document.getElementById("movie_table");
-	  switching = true;
-	  //Set the sorting direction to ascending:
-	  dir = "asc"; 
-	  /*Make a loop that will continue until
-	  no switching has been done:*/
-	  while (switching) {
-	    //start by saying: no switching is done:
-	    switching = false;
-	    rows = table.getElementsByTagName("TR");
-	    /*Loop through all table rows (except the
-	    first, which contains table headers):*/
-	    for (i = 1; i < (rows.length - 1); i++) {
-	      //start by saying there should be no switching:
-	      shouldSwitch = false;
-	      /*Get the two elements you want to compare,
-	      one from current row and one from the next:*/
-	      x = rows[i].getElementsByTagName("TH")[n];
-	      y = rows[i + 1].getElementsByTagName("TH")[n];
-	      /*check if the two rows should switch place,
-	      based on the direction, asc or desc:*/
-	      if (dir == "asc") {
-	        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-	          //if so, mark as a switch and break the loop:
-	          shouldSwitch= true;
-	          break;
-	        }
-	      } else if (dir == "desc") {
-	        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-	          //if so, mark as a switch and break the loop:
-	          shouldSwitch= true;
-	          break;
-	        }
-	      }
-	    }
-	    if (shouldSwitch) {
-	      /*If a switch has been marked, make the switch
-	      and mark that a switch has been done:*/
-	      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-	      switching = true;
-	      //Each time a switch is done, increase this count by 1:
-	      switchcount ++;      
-	    } else {
-	      /*If no switching has been done AND the direction is "asc",
-	      set the direction to "desc" and run the while loop again.*/
-	      if (switchcount == 0 && dir == "asc") {
-	        dir = "desc";
-	        switching = true;
-	      }
-	    }
-	  }
-}
 
 
 jQuery.ajax({
