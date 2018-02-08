eacda2ca4350476390dde71c594d62cc4c8b693f
Lijun Ma <lijunm@uci.edu>
2018-02-07 16:54:36 -0800

    prev/next & itemsPerPage

--- project2/WebContent/search.js
+++ project2/WebContent/search.js
@@ -1,3 +1,9 @@
+
+var movieTableBodyElement = jQuery("#movie_table_body");
+var movieTableHeadElement = jQuery("#movie_head");
+var totalJsonSize, curItemsPerPage;
+var curPage = 0;
+
 function splitListItems(listItems) {
 	console.log(listItems)
 	var array = listItems.split(',');
@@ -7,12 +13,14 @@ function splitListItems(listItems) {
 function handleSearchResult(resultDataString) {
 	console.log(resultDataString);
 	resultDataJson = JSON.parse(JSON.stringify(resultDataString));
+	totalJsonSize = resultDataJson.length;
 	
 	console.log("handle search response");
 	console.log(resultDataJson);
 	//will always direct to movieList.html page, but can be "No Result Found" or movie list
-	var movieTableBodyElement = jQuery("#movie_table_body");
-	var movieTableHeadElement = jQuery("#movie_head");
+
+	var curItemsPerPage = $("#search-count option:selected").val();
+	
 	if (jQuery.isEmptyObject(resultDataJson)) {
 		var rowHTML = "<tr><td><h2>No Result Found</h2></td></tr>";
 		movieTableHeadElement.append(rowHTML);
@@ -20,25 +28,54 @@ function handleSearchResult(resultDataString) {
 	else {
 		var rowHTML = "<tr><th>ID</th><th onclick=\"sortTable(1)\"><a href=\"#\">Title</a></th><th onclick=\"sortTable(2)\"><a href=\"#\">Year</a></th><th>Director</th><th>Genres</th><th>Stars</th></tr>";
 		movieTableHeadElement.append(rowHTML);
-		for (var i = 0; i < resultDataJson.length; i++) {
-			var rowHTML = "";
-			rowHTML += "<tr>";
-			rowHTML += "<th>" + resultDataJson[i]["movie_id"] + "</th>";
-			rowHTML += "<th><form id=\"title_form"+i+"\" action=\"/project2/singlemovie.html\" method=\"get\"><input type=\"hidden\" value=\""+resultDataJson[i]["movie_title"]+"\" name=\"title\"><a href=\"#\" onclick=\"document.getElementById('title_form"+i+"').submit();\" id = \"title_achor\">"+resultDataJson[i]["movie_title"]+"</a></form></th>";
-			rowHTML += "<th>" + resultDataJson[i]["movie_year"] + "</th>";
-			rowHTML += "<th>" + resultDataJson[i]["movie_director"] + "</th>";
-			rowHTML += "<th>" + resultDataJson[i]["movie_genres"] + "</th>";
-			rowHTML += "<th>";
-			var star_arr = splitListItems(resultDataJson[i]["movie_stars"]);
-			for (var j = 0; j < star_arr.length; j++) {
-				rowHTML += "<form id='star_form"+j+"' action=\"/project2/singlestar.html\" method=\"get\"><input type=\"hidden\" value=\""+star_arr[j]+"\" name=\"star\"><a href=\"#\" onclick=\"document.getElementById('star_form"+j+"').submit();\">"+star_arr[j]+"</a></form>"
-			}
-			rowHTML += "</th>";
-			rowHTML += "</tr>"
-			movieTableBodyElement.append(rowHTML);
+		
+		// show items per page in html
+		showPage(curItemsPerPage);
+	}	
+}
+
+
+
+
+function showPage(itemsPerPage) {
+	$( "#movie_table_body" ).empty();
+	curItemsPerPage = itemsPerPage;
+	var rowHTML = "";
+	for (var i = curPage * curItemsPerPage, k = 0; k < curItemsPerPage && i < totalJsonSize; k++, i++) {
+		rowHTML = "";
+		rowHTML += "<tr>";
+		rowHTML += "<th>" + resultDataJson[i]["movie_id"] + "</th>";
+		rowHTML += "<th><form id=\"title_form"+i+"\" action=\"/project2/singlemovie.html\" method=\"get\"><input type=\"hidden\" value=\""+resultDataJson[i]["movie_title"]+"\" name=\"title\"><a href=\"#\" onclick=\"document.getElementById('title_form"+i+"').submit();\" id = \"title_achor\">"+resultDataJson[i]["movie_title"]+"</a></form></th>";
+		rowHTML += "<th>" + resultDataJson[i]["movie_year"] + "</th>";
+		rowHTML += "<th>" + resultDataJson[i]["movie_director"] + "</th>";
+		rowHTML += "<th>" + resultDataJson[i]["movie_genres"] + "</th>";
+		rowHTML += "<th>";
+		var star_arr = splitListItems(resultDataJson[i]["movie_stars"]);
+		for (var j = 0; j < star_arr.length; j++) {
+			rowHTML += "<form id='star_form"+j+"' action=\"/project2/singlestar.html\" method=\"get\"><input type=\"hidden\" value=\""+star_arr[j]+"\" name=\"star\"><a href=\"#\" onclick=\"document.getElementById('star_form"+j+"').submit();\">"+star_arr[j]+"</a></form>"
 		}
+		rowHTML += "</th>";
+		rowHTML += "</tr>"
+		movieTableBodyElement.append(rowHTML);
+		
 	}
 }
+
+
+function prevPage() {
+	if (curPage > 0) {
+		curPage--;
+	}
+	showPage(curItemsPerPage);
+}
+
+function nextPage() {
+	if ((curPage + 1) * curItemsPerPage < totalJsonSize) {
+		curPage++;
+	}
+	showPage(curItemsPerPage) ;
+}
+
 
 function getQueryString() {
 	var result = {}, queryString = location.search.slice(1), re = /([^&=]+)=([^&]*)/g, m;
